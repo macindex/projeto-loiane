82bdd821967c4cdbbc0db09caea8dc348a441ff7
marcio lira <machollanda@gmail.com>
2020-09-09 21:07:56 -0300

    arquivos iniciais do projeto

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'meu-primeiro-component',//utilização de web components
+	template: `<p>Meu primeiro componente com angular 2!</p>`//template literal
+})
+export class MeuPrimeiroComponent { }
