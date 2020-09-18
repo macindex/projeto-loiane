c56389a0c782ecbcca20a2038b676714f2651b1f
marcio lira <machollanda@gmail.com>
2020-09-18 16:59:40 -0300

    services and dependence injection

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
@@ -10,14 +11,15 @@ export class CursosComponent implements OnInit {
   
   nomePortal: string;
 
-  cursos: string[] = ['Java', 'Angular', 'Grunt'];
+  cursos: string[];
 
-  constructor() { 
+  constructor(private cursosService: CursosService) { 
     this.nomePortal = 'http://loiane.training';
 
-    for (let i=0; i<this.cursos.length; i++){
-      let curso = this.cursos[i];
-    }
+    // for (let i=0; i<this.cursos.length; i++){
+    //   let curso = this.cursos[i];
+    // var servico = new CursosService
+    this.cursos = this.cursosService.getCursos();
 }  
   ngOnInit(): void {
   }
